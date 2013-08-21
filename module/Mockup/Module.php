9ea563479c6ebf1199ce7c8edd8bec0269da2f0a
Jurian Sluiman <jurian@soflomo.com>
2013-08-21 23:48:08 +0200

    Remove unused mockup pages

--- module/Mockup/Module.php
+++ module/Mockup/Module.php
@@ -10,24 +10,4 @@ class Module
     {
         return include __DIR__ . '/config/module.config.php';
     }
-
-    // public function onBootstrap($e)
-    // {
-    //     $app = $e->getApplication();
-    //     $em  = $app->getEventManager()->getSharedManager();
-    //     $sm  = $app->getServiceManager();
-
-    //     $config = $sm->get('Config');
-    //     //$list   = $config['my_controller_list'];
-    //     $list   = (array) 'Soflomo\Blog\Controller\ArticleController';
-    //     $loader = $sm->get('ControllerLoader');
-
-    //     $em->attach('Soflomo\Prototype\Controller\PrototypeController', MvcEvent::EVENT_DISPATCH, function ($e) use ($list, $loader) {
-    //         foreach ($list as $name) {
-    //             $controller = $loader->get($name);
-
-    //             $e->getTarget()->getEventManager()->attach(MvcEvent::EVENT_DISPATCH, array($controller, 'onDispatch'));
-    //         }
-    //     }, -100);
-    // }
 }
--- module/Mockup/config/module.config.php
+++ module/Mockup/config/module.config.php
@@ -2,34 +2,6 @@
 return array(
     'soflomo_prototype' => array(
         'pages' => array(
-            'blog' => array(
-                'route'    => '/',
-                'template' => 'mockup/home',
-            ),
-            'blog-post1' => array(
-                'route'    => '/interface-injection-with-initializers-in-zend-servicemanager',
-                'template' => 'mockup/post1',
-            ),
-            'blog-post2' => array(
-                'route'    => '/using-zend-framework-service-managers-in-your-application',
-                'template' => 'mockup/post2',
-            ),
-            'blog-post3' => array(
-                'route'    => '/use-soflomo-prototype-for-quick-website-prototypes',
-                'template' => 'mockup/post3',
-            ),
-            'blog-post4' => array(
-                'route'    => '/strategies-for-hydrators-a-practical-use-case',
-                'template' => 'mockup/post4',
-            ),
-            'blog-post5' => array(
-                'route'    => '/factories-for-translator-loaders',
-                'template' => 'mockup/post5',
-            ),
-            'blog-post6' => array(
-                'route'    => '/the-importance-of-being-away',
-                'template' => 'mockup/post6',
-            ),
             'about-me' => array(
                 'route'    => '/about',
                 'template' => 'mockup/me',
@@ -37,35 +9,9 @@ return array(
         ),
     ),
 
-    'navigation' => array(
-        'default' => array(
-            array(
-                'label' => 'Blog',
-                'route' => 'blog',
-
-                'pages' => array(
-                    array(
-                        'label' => 'Interface injection with initializers in Zend\ServiceManager',
-                        'route' => 'blog-post1'
-                    ),
-                    array(
-                        'label' => 'Interface injection with initializers in Zend\ServiceManager 2',
-                        'route' => 'blog-post2'
-                    ),
-                ),
-            ),
-        ),
-    ),
-
     'view_manager' => array(
         'template_path_stack' => array(
             __DIR__ . '/../view',
-        ),
-    ),
-
-    'service_manager' => array(
-        'factories' => array(
-            'default' => 'Zend\Navigation\Service\DefaultNavigationFactory',
         ),
     ),
 );
